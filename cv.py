6352868b2480005715f83568703fe6b3d19410b9
Andrew Wade <andrew@andrewwade.co.uk>
2014-09-01 19:23:13 +0100

    refactor code in to a Main class

--- cv.py
+++ cv.py
@@ -57,8 +57,6 @@ class AppConfig(object):
     throughput_wait_secs = 1
     proc_names = []
 
-config = AppConfig()
-
 def moving_average(iterable, n=3):
     # moving_average([40, 30, 50, 46, 39, 44]) --> 40.0 42.0 45.0 43.0
     # http://en.wikipedia.org/wiki/Moving_average
@@ -80,14 +78,9 @@ def format_size(n):
         n /= 1024.0
     return fmt % (n, 'TB')
 
-def nprint(mainwin, str):
-    if config.monitor or config.monitor_continuous:
-        mainwin.addstr(str)
-    else:
-        print(str, end="")
-
 def get_pids():
     return [e for e in os.listdir('/proc') if e.isdigit() and os.path.isdir('/proc/'+e)]
+
 
 def find_pids_by_binary_name(bin_name, max_pids=None):
     """
@@ -140,8 +133,9 @@ def find_fd_for_pid(pid, max_fd=None):
             break
     return fds
 
-def get_fdinfo(pid, fdnum):
-    fdpath = "%s/%d/fd/%d" % (PROC_PATH, pid, fdnum)
+def get_fdinfo(pid, fdnum, proc_path=PROC_PATH):
+    assert proc_path
+    fdpath = "%s/%d/fd/%d" % (proc_path, pid, fdnum)
 
     fdinfo = FdInfo()
     fdinfo.num = fdnum
@@ -158,201 +152,213 @@ def get_fdinfo(pid, fdnum):
 
     fdinfo.pos = 0
     fdinfo.tv = time()
-    fdinfo.pos = int(open("%s/%d/fdinfo/%d" % (PROC_PATH, pid, fdnum)).readlines()[0].strip().split("\t")[1])
+    fdinfo.pos = int(open("%s/%d/fdinfo/%d" % (proc_path, pid, fdnum)).readlines()[0].strip().split("\t")[1])
     return fdinfo
 
-def parse_options():
-    """
-    Usage: ./cv [-vqwmMh] [-W] [-c command]
-  -v --version            show version
-  -q --quiet              hides some warning/error messages
-  -w --wait               estimate I/O throughput and ETA (slower display)
-  -W --wait-delay secs    wait 'secs' seconds for I/O estimation (implies -w, default=1.0)
-  -m --monitor            loop while monitored processes are still running
-  -M --monitor-continuous  like monitor but never stop (similar to watch ./cv)
-  -h --help               this message
-  -c --command cmd        monitor only this command name (ex: firefox)
-  """
-    ap = argparse.ArgumentParser(description=APP_NAME)
-    ap.add_argument('-v', '--version', action='store_true', help='show version')
-    ap.add_argument('-q', '--quiet', action='store_true', help='hides some warning/error messages')
-    ap.add_argument('-w', '--wait', action='store_true', help='estimate I/O throughput and ETA (slower display)')
-    ap.add_argument('-W', '--wait-delay', type=int, metavar='<secs>', help='''wait 'secs' seconds for I/O estimation (implies -w, default=1.0)''')
-    ap.add_argument('-c', '--commands', action='append', help='commands to monitor')
-    ap.add_argument('-m', '--monitor', action='store_true', help='loop while monitored processes are still running')
-    ap.add_argument('-M', '--monitor-continuous', action='store_true', help='like monitor but never stop (similar to watch ./cv)')
-    args = ap.parse_args()
 
-    # -w implies -W
-    if args.wait_delay is not None:
-        args.wait = True
-    return args
+class Main(object):
+    def nprint(self, str):
+        if self.config.monitor or self.config.monitor_continuous:
+            self.mainwin.addstr(str)
+        else:
+            print(str, end="")
 
-def monitor_processes(throughputs=None, mainwin=None):
-    """
-    Find pids whose binary names (i.e. basename of path) match those in config.proc_name.
+    def parse_options(self):
+        """
+        Usage: ./cv [-vqwmMh] [-W] [-c command]
+      -v --version            show version
+      -q --quiet              hides some warning/error messages
+      -w --wait               estimate I/O throughput and ETA (slower display)
+      -W --wait-delay secs    wait 'secs' seconds for I/O estimation (implies -w, default=1.0)
+      -m --monitor            loop while monitored processes are still running
+      -M --monitor-continuous  like monitor but never stop (similar to watch ./cv)
+      -h --help               this message
+      -c --command cmd        monitor only this command name (ex: firefox)
+      """
+        ap = argparse.ArgumentParser(description=APP_NAME)
+        ap.add_argument('-v', '--version', action='store_true', help='show version')
+        ap.add_argument('-q', '--quiet', action='store_true', help='hides some warning/error messages')
+        ap.add_argument('-w', '--wait', action='store_true', help='estimate I/O throughput and ETA (slower display)')
+        ap.add_argument('-W', '--wait-delay', type=int, metavar='<secs>', help='''wait 'secs' seconds for I/O estimation (implies -w, default=1.0)''')
+        ap.add_argument('-c', '--commands', action='append', help='commands to monitor')
+        ap.add_argument('-m', '--monitor', action='store_true', help='loop while monitored processes are still running')
+        ap.add_argument('-M', '--monitor-continuous', action='store_true', help='like monitor but never stop (similar to watch ./cv)')
+        args = ap.parse_args()
 
-    """
-    if throughputs is None:
-        raise ValueError("throughputs is None")
-    if not config.proc_names:
-        raise ValueError("no proc names defined")
-    fd_count = 0
-    result_count = 0
-    results = []
+        # -w implies -W
+        if args.wait_delay is not None:
+            args.wait = True
+        return args
 
-    pidinfos = []
+    def monitor_processes(self):
+        """
+        Find pids whose binary names (i.e. basename of path) match those in config.proc_name.
 
-    for name in config.proc_names:
-        p = find_pids_by_binary_name(name)
-        if p:
-            pidinfos.extend(p)
+        """
+        if not self.config.proc_names:
+            raise ValueError("no proc names defined")
+        fd_count = 0
+        result_count = 0
+        results = []
 
-    pidinfos = pidinfos[:MAX_PIDS]
-    if not pidinfos:
-        if config.quiet:
+        pidinfos = []
+
+        for name in self.config.proc_names:
+            p = find_pids_by_binary_name(name)
+            if p:
+                pidinfos.extend(p)
+
+        pidinfos = pidinfos[:MAX_PIDS]
+        if not pidinfos:
+            if self.config.quiet:
+                return 0
+            if self.config.curses:
+                self.mainwin.clear()
+                self.mainwin.refresh()
+            self.nprint("No command currently running: %s. exiting" % (", ".join(self.config.proc_names)))
             return 0
+
+        for pidinfo in pidinfos:
+            fds = find_fd_for_pid(pidinfo.pid)
+            fds = fds[:MAX_FD_PER_PID]
+            fd_size_max = 0
+            fd_biggest = 0
+            if not fds:
+                nprint("[%5d] %s inactive/flushing/streaming/...\n" % (pidinfo.pid, pidinfo.name))
+                # FIXME: why is this needed here?
+                if self.config.curses:
+                    self.mainwin.refresh()
+                continue
+            for fd in fds:
+                fd_info = get_fdinfo(pidinfo.pid, fd)
+                if fd_info.size > fd_size_max:
+                    fd_size_max = fd_info.size
+                    fd_biggest = fd_info
+
+            # We've got our biggest_fd now, let's store the result
+            result = Result()
+            result.pid = pidinfo
+            result.fd = fd_biggest
+            result.hbegin = None
+            result.hend = None
+            result.hsize = 0
+            results.append(result)
+
+        # wait a bit, so we can estimate throughput
+        if self.config.throughput:
+            sleep(self.config.throughput_wait_secs)
         if config.curses:
-            mainwin.clear()
-            mainwin.refresh()
-        nprint(mainwin, "No command currently running: %s. exiting" % (", ".join(config.proc_names)))
-        return 0
+            self.mainwin.clear()
+            self.mainwin.refresh()
 
-    for pidinfo in pidinfos:
-        fds = find_fd_for_pid(pidinfo.pid)
-        fds = fds[:MAX_FD_PER_PID]
-        fd_size_max = 0
-        fd_biggest = 0
-        if not fds:
-            nprint(mainwin, "[%5d] %s inactive/flushing/streaming/...\n" % (pidinfo.pid, pidinfo.name))
-            # FIXME: why is this needed here?
-            if config.curses:
-                mainwin.refresh()
-            continue
-        for fd in fds:
-            fd_info = get_fdinfo(pidinfo.pid, fd)
-            if fd_info.size > fd_size_max:
-                fd_size_max = fd_info.size
-                fd_biggest = fd_info
+        for result in results:
+            progress_pcnt = 0
+            cur_fd_info = None
 
-        # We've got our biggest_fd now, let's store the result
-        result = Result()
-        result.pid = pidinfo
-        result.fd = fd_biggest
-        result.hbegin = None
-        result.hend = None
-        result.hsize = 0
-        results.append(result)
+            fd = result.fd
+            if self.config.throughput:
+                cur_fd_info = get_fdinfo(result.pid.pid, result.fd.num)
+                if cur_fd_info.name == result.fd.name:
+                    fd = cur_fd_info
+                else:
+                    cur_fd_info = None
 
-    # wait a bit, so we can estimate throughput
-    if config.throughput:
-        sleep(config.throughput_wait_secs)
-    if config.curses:
-        mainwin.clear()
-        mainwin.refresh()
-    
-    for result in results:
-        progress_pcnt = 0
-        cur_fd_info = None
+            if fd.pos > 0.0 and fd.size > 0.0:
+                progress_pcnt = float(fd.pos)/fd.size
+            self.nprint("[%5d] %s %s %.1f%% (%s / %s)" % (
+                result.pid.pid,
+                result.pid.name,
+                fd.name,
+                progress_pcnt,
+                format_size(float(fd.pos)),
+                format_size(float(fd.size))))
 
-        fd = result.fd
-        if config.throughput:
-            cur_fd_info = get_fdinfo(result.pid.pid, result.fd.num)
-            if cur_fd_info.name == result.fd.name:
-                fd = cur_fd_info
-            else:
-                cur_fd_info = None
+            if self.config.throughput and cur_fd_info:
+                bytes_per_sec = 0
+                sec_diff = float(fd.tv) - result.fd.tv
+                byte_diff = fd.pos - result.fd.pos
+                tkey = (result.pid.pid, fd.num)
+                self.throughputs[tkey] = self.throughputs[tkey][:THROUGHPUT_SAMPLE_SIZE-1]
+                self.throughputs[tkey].append(byte_diff/sec_diff)
+                self.throughput_moving_avg = list(moving_average(self.throughputs[tkey]))
+                if throughput_moving_avg:
+                    bytes_per_sec = throughput_moving_avg.pop()
+                self.nprint(" %s/s" % format_size(bytes_per_sec))
+            self.nprint("\n")
 
-        if fd.pos > 0.0 and fd.size > 0.0:
-            progress_pcnt = float(fd.pos)/fd.size
-        nprint(mainwin, "[%5d] %s %s %.1f%% (%s / %s)" % (
-            result.pid.pid,
-            result.pid.name,
-            fd.name,
-            progress_pcnt,
-            format_size(float(fd.pos)),
-            format_size(float(fd.size))))
+        return results
 
-        if config.throughput and cur_fd_info:
-            bytes_per_sec = 0
-            sec_diff = float(fd.tv) - result.fd.tv
-            byte_diff = fd.pos - result.fd.pos
-            tkey = (result.pid.pid, fd.num)
-            throughputs[tkey] = throughputs[tkey][:THROUGHPUT_SAMPLE_SIZE-1]
-            throughputs[tkey].append(byte_diff/sec_diff)
-            throughput_moving_avg = list(moving_average(throughputs[tkey]))
-            if throughput_moving_avg:
-                bytes_per_sec = throughput_moving_avg.pop()
-            nprint(mainwin, " %s/s" % format_size(bytes_per_sec))
-        nprint(mainwin, "\n")
+    def make_config(self):
+        config = AppConfig()
+        args = self.parse_options()
+        if args.quiet:
+            config.quiet = True
+        if args.commands:
+            config.proc_names = args.commands
+        else:
+            config.proc_names = PROC_NAMES
+        if args.wait:
+            config.throughput = True
+        if args.monitor:
+            config.monitor = True
+        if args.monitor_continuous:
+            config.monitor_continuous = True
+        if args.wait_delay:
+            config.throughput = True
+            config.throughput_wait_secs = int(args.wait_delay)
+        else:
+            config.throughput_wait_secs = 1
+        if config.monitor or config.monitor_continuous:
+            config.curses = True
+        return config
 
-    return results
+    def endwin(self):
+        if not self.mainwin:
+            return
+        curses.nocbreak()
+        self.mainwin.keypad(0)
+        curses.echo()
+        curses.endwin()
 
-def make_config():
-    args = parse_options()
-    if args.quiet:
-        config.quiet = True
-    if args.commands:
-        config.proc_names = args.commands
-    else:
-        config.proc_names = PROC_NAMES
-    if args.wait:
-        config.throughput = True
-    if args.monitor:
-        config.monitor = True
-    if args.monitor_continuous:
-        config.monitor_continuous = True
-    if args.wait_delay:
-        config.throughput = True
-        config.throughput_wait_secs = int(args.wait_delay)
-    else:
-        config.throughput_wait_secs = 1
-    if config.monitor or config.monitor_continuous:
-        config.curses = True
-
-def endwin(window):
-    if not window:
-        return
-    curses.nocbreak()
-    window.keypad(0)
-    curses.echo()
-    curses.endwin()
-
-def main():
-    make_config()
-    mainwin = None
-
-    if config.curses:
-        mainwin = curses.initscr()
-
-    def int_handler(signum, frame):
-        if mainwin is not None:
+    def int_handler(self, signum, frame):
+        if self.mainwin is not None:
             try:
-                endwin(mainwin)
+                self.endwin()
             except curses.error as ce:
                 print(ce, file=sys.stderr)
         sys.exit(0)
-    signal.signal(signal.SIGINT, int_handler)
 
-    throughputs = defaultdict(list)
-    try:
-        if config.monitor or config.monitor_continuous:
-            results = []
-            while True:
-                results = monitor_processes(mainwin=mainwin, throughputs=throughputs)
-                mainwin.refresh()
-                if config.monitor_continuous and not results:
-                    sleep(config.throughput_wait_secs)
-                if not ((config.monitor and results) or config.monitor_continuous):
-                    break
-        else:
-            monitor_processes(throughputs=throughputs)
-    finally:
+    def main(self):
+        self.config = self.make_config()
+
+        if self.config.curses:
+            self.mainwin = curses.initscr()
+
+        signal.signal(signal.SIGINT, self.int_handler)
+
         try:
-            if mainwin:
-                endwin(mainwin)
-        except curses.error as ce:
-            print(ce, file=sys.stderr)
+            if self.config.monitor or self.config.monitor_continuous:
+                results = []
+                while True:
+                    results = self.monitor_processes()
+                    self.mainwin.refresh()
+                    if self.config.monitor_continuous and not results:
+                        sleep(self.config.throughput_wait_secs)
+                    if not ((self.config.monitor and results) or self.config.monitor_continuous):
+                        break
+            else:
+                self.monitor_processes()
+        finally:
+            try:
+                if self.mainwin:
+                    self.endwin()
+            except curses.error as ce:
+                print(ce, file=sys.stderr)
+
+    def __init__(self):
+        self.config = None
+        self.mainwin = None
+        self.throughputs = defaultdict(list)
 
 if __name__ == '__main__':
-    main()
+    Main().main()
